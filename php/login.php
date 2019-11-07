42c2a7d57b407f6baf9aa71763d9756159ea41d4
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-11-07 10:11:14 +0530

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,9 +3,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-	<link rel="stylesheet" href="../css/user_login.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<meta charset="utf-8">
-	<title>login</title>
+	<title>Login</title>
 	<link rel="stylesheet" href="">
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,18 +20,30 @@
 
 
 </head>
-<body>
+<body style="background-image: url('https://www.solidbackgrounds.com/images/website/950x534/950x534-blue-abstract-noise-free-website-background-image.jpg');">
 	<header>
-		<div class="main">
+	<div class ="card border-primary mb-3" style="max-width: 30rem; margin:auto; top:8rem; background-color: ;" >
+		<div class="card-header" style="text-align: center">
+			Login
+		</div>
+		<!-- <h3 class="text-center p-4">/h3> -->
+		<div class="card-body">
 			<form class="box" action="manager.php" method="post">
-				<h1>LOG IN</h1>
-				<br><input type="email" name="JS_email" placeholder="Username" required="" id="">
-				<input type="password" name="password" placeholder="Password" required="" id=""><br>
-				<input type="submit" name="login" value="Log in" >
-
+			<div class="form-group">
+				<input type="email" name="JS_email" class = "form-control" placeholder="Username" required="" id="">
+			</div>
+			<div class="form-group">
+				<input type="password" name="password" class = "form-control" placeholder="Password" required="" id=""><br>
+			</div>
+			<div class="form-group" style="text-align: center;">
+				<input type="submit" name="login" class="btn btn-primary"value="Login" >
+			</div>
 				<p>Don't have an account ? <a href="signup.php"> Sign Up Here</a></p>
 			</form>
+			</div>
 		</div>
+	</div>
+</div>
 	</header>
 </body>
 </html>
