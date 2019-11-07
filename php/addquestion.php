f298581ce1176e6b93c021ad3d03774d93c598bd
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-11-07 11:24:06 +0530

    add question css

--- php/addquestion.php
+++ php/addquestion.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
 	<title>Add New Question</title>
 </head>
-<body>
+<body style="background-color: #DADEDF;">
+	<?php include 'navbar.php';?>
+	<div class ="card border-primary mb-3" style="max-width: 30rem; margin:auto; top:8rem; background-color: ;" >
+			<div class="card-header" style="text-align: center">
+				Add New Question
+			</div>
+			<div class="card-body">
 		<form class="box" action="manager.php" method="post">
 			<div class="form-group">
 				<input type="text" name="subject" class = "form-control" placeholder="Subject" required="" id="">
@@ -18,5 +32,6 @@
 				<input type="submit" name="addquestion" class="btn btn-primary"value="Submit" >
 			</div>
 		</form>
+		</div>
 </body>
 </html>
